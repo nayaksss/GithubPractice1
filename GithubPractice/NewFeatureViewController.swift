0770a80af384014d45285d3f45a3f54c6a263ad1
vinayak <tuptewar7@gmail.com>
2020-10-23 13:48:12 +0530

    added label, viewController

---
+++ GithubPractice/NewFeatureViewController.swift
@@ -0,0 +1,31 @@
+//
+//  NewFeatureViewController.swift
+//  GithubPractice
+//
+//  Created by Vinayak Balaji Tuptewar on 23/10/20.
+//  Copyright © 2020 Vinayak Balaji Tuptewar. All rights reserved.
+//
+
+import UIKit
+
+class NewFeatureViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
